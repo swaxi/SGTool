462b705d1f604b29cd96f03b69e73f337d1e95e4
Mark Jessell <00073294@uwa.edu.au>
2025-08-06 11:19:08 +0800

    first inclusion of onecode code

---
+++ flows/sgtool.py
@@ -0,0 +1,23 @@
+# This file is your entry point:
+# - add you Python files and folder inside this 'flows' folder
+# - add your imports
+# - just don't change the name of the function 'run()' nor this filename ('sgtool.py')
+#   and everything is gonna be ok.
+#
+# Remember: everything is gonna be ok in the end: if it's not ok, it's not the end.
+# Alternatively, ask for help at https://github.com/deeplime-io/onecode/issues
+
+import onecode
+from onecode import Logger, text_input
+
+
+def run():
+    onecode.Logger.info(
+        """
+        #####################################################################
+        ###> Hello from SGTool!
+        ###> Fill in this run() function with something awesome!
+        #####################################################################
+        """
+    )
+    Logger.info(f"Hello {text_input('your name', 'OneCoder')}!")
